b044e751fec33c91a3e72c858ef9e61a2b95e438
samyyuv <samyy.uv@gmail.com>
2022-07-12 12:06:11 +0200

    adding images to the generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,25 @@
 import React from "react";
+import memesData from "../memesData.js"
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState("");
 
+  function getImage() {
+    const memeArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memeArray.length)
+    const randomUrl = memeArray[randomNumber].url
+    setMeme(randomUrl)
+  }
   return (
     <div className="memeContent">
       <div className="inputContainer">
         <input className="inputMeme" type="text" />
         <input className="inputMeme" type="text" />
       </div>
-      <button className="submitMeme" >Get a new meme image 🖼️</button>
-
+      <button className="submitMeme" onClick={getImage}>Get a new meme image 🖼️</button>
+      <div className="memeContainer">
+        <img src={meme} className="img" />
+      </div>
     </div>
   )
 }
